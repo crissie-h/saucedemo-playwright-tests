17eb2efbfcd16a44cb4eb2028388923855eb4da5
crissie-h <crissie.melo@hotmail.com>
2026-01-30 16:35:26 -0300

    testData name change, login first test

--- tests/specs/Login.spec.ts
+++ tests/specs/Login.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../../pages/LoginPage';
+import { USERS } from '../../utils/testData.ts';
 
 test.describe('Login Tests', () => {
     let loginPage: LoginPage;
@@ -8,5 +9,19 @@ test.describe('Login Tests', () => {
         loginPage = new LoginPage(page);
         await loginPage.goto();
     });
+
+    test('TC001 - Successful login using standard user', async ({ page }) => {
+        await loginPage.login(USERS.STANDARD.username, USERS.STANDARD.password);
+    });
+    
+    // test('TC002 - Invalid cridentials error', async ({ page }) => {
+
+    // })
+
+
+
+
+
+
 });
 
